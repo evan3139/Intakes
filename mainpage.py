c4d16f4dd515077c55150a8f7fa1e8fd43bd86ee
evan3139 <ewildenhain99@gmail.com>
2018-12-11 01:52:42 -0500

    Add files via upload

--- mainpage.py
+++ mainpage.py
@@ -24,6 +24,10 @@ def combine_all_sheets(file):
     fileNames = os.listdir(directory)
     files_temp = []
     files_scores_temp = []
+
+    newPath = 'C:/VantagePoint/Full-Intakes/'
+    if not os.path.exists(newPath):
+        os.makedirs(newPath)
     if len(fileNames) <= 2:
         raise Exception("Please enter a valid number (Digit not word) For Questions")
         sys.exit("Error")
@@ -41,12 +45,14 @@ def combine_all_sheets(file):
     files_scores = [pd.ExcelFile(name) for name in files_scores_temp]
 
     # turn them into dataframes
-    frames = [x.parse(x.sheet_names[0], header=None, index_col=None) for x in files]
+    frames = [x.parse(x.sheet_names[0], header=None, index_col=None, na_filter = False) for x in files]
     # delete the first row for all frames except the first
     # i.e. remove the header row -- assumes it's the first
     frames[1:] = [df[1:] for df in frames[1:]]
 
-    frames_scores = [x.parse(x.sheet_names[0], header=None, index_col=None) for x in files_scores]
+    # Make sure to do NA_FILTER = FALSE. This will make sure any and all N/A stays N/A rather than becoming an empty
+    # cell.
+    frames_scores = [x.parse(x.sheet_names[0], header=None, index_col=None, na_filter = False) for x in files_scores]
     frames_scores[1:] = [df[1:] for df in frames_scores[1:]]
 
     # concatenate them..
@@ -54,12 +60,40 @@ def combine_all_sheets(file):
     combined_scores = pd.concat(frames_scores)
 
     # Combines the excel sheets
-    combined.to_excel(directory + "/" + "Full Intakes.xlsx", header=False, index=False)
-    combined_scores.to_excel(directory + "/" + "Full Intakes-Scores.xlsx", header=False, index=False)
+    combined.to_excel(newPath + "Full Intakes.xlsx", header=False, index=False, na_rep='NA')
+    combined_scores.to_excel(newPath + "Full Intakes-Scores.xlsx", header=False, index=False, na_rep='NA')
 
     # Resize the columns for the two new excel sheets.
-    resize_columns(directory + "/" + "Full Intakes.xlsx")
-    resize_columns(directory + "/" + "Full Intakes-Scores.xlsx")
+    resize_columns(newPath + "Full Intakes.xlsx")
+    resize_columns(newPath + "Full Intakes-Scores.xlsx")
+
+    # Alter the scores only to be sorted by age. After we will resort it to be altered by a custom key for race We
+    # had to remove the first row, due to Name Age etc messing up the sorting method, and i couldnt find a work
+    # around to skip the first row.
+    sort_age = combined_scores.iloc[1:].copy()
+    # Create sort race for when we will use it in 5 minutes.( 1 millisecond when running)
+    sort_race = sort_age.copy()
+    # Sorts all the ages in the databse without having to transfer.
+    sort_age.sort_values(2, inplace=True)
+    # This will find the amount of rows and subtract 1 so we keep the first row as (:-22) will remove 22 rows
+    # starting from the bottom.
+    drop = len(combined_scores) - 1
+    top_of_file = combined_scores[:-drop]
+    # Append it to sort_age
+    sort_age = top_of_file.append(sort_age)
+    # Create a new Intake For age Sort
+    sort_age.to_excel(newPath + "Full Intakes-Scores-AgeSort.xlsx", header=False, index=False, na_rep='NA')
+    resize_columns(newPath + "Full Intakes-Scores-AgeSort.xlsx")
+
+    sort_race[4] = pd.Categorical(sort_race[4], ["W", "B", "L", "A", "NA", "O", "N/A"])
+    sort_race.sort_values(4, inplace=True)
+    sort_race = top_of_file.append(sort_race)
+
+    sort_race.to_excel(newPath + "Full Intakes-Scores-RaceSort.xlsx", header=False, index=False, na_rep='NA')
+    resize_columns(newPath + "Full Intakes-Scores-RaceSort.xlsx")
+
+    # combined_scores.to_excel(directory + "/" + "Full Intakes-Scores.xlsx", header=False, index=False, na_rep='NA')
+    # resize_columns(directory + "/" + "Full Intakes-Scores-AgeSort.xlsx")
 
     sys.exit(0)
 
@@ -228,7 +262,8 @@ def create_docx_template(excel_name, docx_name, file_title):
 
     # Fills the file with ID's genders, ages, races in a template format.
     for index, i in enumerate(names):
-        doc.add_paragraph(genders[index] + ',' + str(ages[index]) + "," + races[index] + "," + names[index] + ":")
+        doc.add_paragraph(
+            str(genders[index]) + ',' + str(ages[index]) + "," + str(races[index]) + "," + str(names[index]) + ":")
     doc.save(docx_name)
 
 
