f4719be1082632b4dc40aaf13b53da33d07ca61f
evan3139 <ewildenhain99@gmail.com>
2019-05-04 21:46:28 -0400

    Changed the title from week to topics.

---
+++ Resize Column.py
@@ -0,0 +1,22 @@
+import openpyxl
+from openpyxl.utils import get_column_letter
+
+
+def resize_columns(excel_name):
+    # This reopens the excel file but in the openpyxl library allowing us to alter column lengths
+    wb = openpyxl.load_workbook(excel_name)
+    worksheet = wb.active
+
+    for col in worksheet.columns:
+        max_length = 0
+        column = col[0].column  # Get the Column Name Here
+        for cell in col:
+            try:  # Needed to avoid empty cell errors
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2) * 1.2
+        worksheet.column_dimensions[get_column_letter(column)].width = adjusted_width
+
+    wb.save(excel_name)
--- mainpage.py
+++ mainpage.py
@@ -11,6 +11,7 @@ import xlrd
 import xlsxwriter
 from docx import Document
 from Demographics import *
+from Resize Column import *
 
 
 def is_number(string):
@@ -212,26 +213,6 @@ def score_sheet(worksheet, worksheet_scores, filename, row):
             sheet.write(row, index, int(x))
         else:
             sheet.write(row, index, x)
-
-
-def resize_columns(excel_name):
-    # This reopens the excel file but in the openpyxl library allowing us to alter column lengths
-    wb = openpyxl.load_workbook(excel_name)
-    worksheet = wb.active
-
-    for col in worksheet.columns:
-        max_length = 0
-        column = col[0].column  # Get the Column Name Here
-        for cell in col:
-            try:  # Needed to avoid empty cell errors
-                if len(str(cell.value)) > max_length:
-                    max_length = len(cell.value)
-            except:
-                pass
-        adjusted_width = (max_length + 2) * 1.2
-        worksheet.column_dimensions[get_column_letter(column)].width = adjusted_width
-
-    wb.save(excel_name)
 
 
 def create_docx_template(excel_name, docx_name, file_title):
