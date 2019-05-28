3a6315772a8d4177629a8890d876b34dfdb5f220
evan3139 <ewildenhain99@gmail.com>
2019-05-28 12:55:00 -0400

    Doing stuff with quiz

--- quiz_combined.py
+++ quiz_combined.py
@@ -49,50 +49,47 @@ def quiz_combined(file):
             data[column] = df[4]
             column+= 1
 
-    print(data)
+    data = data.reset_index(drop=True)
+    new_header = data.iloc[0]  # grab the first row for the header
+    data = data[1:]  # take the data less the header row
+    data.columns = new_header  # set the header row as the df header
+    data.drop(data.tail(1).index, inplace=True) # drops last row which is empty
+
     # Make sure to do NA_FILTER = FALSE. This will make sure any and all N/A stays N/A rather than becoming an empty
     # cell.
-
-
-    # concatenate them..
-    combined = pd.concat(frames)
-
     # Combines the excel sheets
-    combined.to_excel(newPath + "Full Intakes.xlsx", header=False, index=False, na_rep='NA')
-    combined_scores.to_excel(newPath + "Full Intakes-Scores.xlsx", header=False, index=False, na_rep='NA')
+    data.to_excel(newPath + "Full Quizzes.xlsx", header=True, index=False, na_rep='NA')
 
     # Resize the columns for the two new excel sheets.
-    resize_columns(newPath + "Full Intakes.xlsx")
-    resize_columns(newPath + "Full Intakes-Scores.xlsx")
+    resize_columns(newPath + "Full Quizzes.xlsx")
 
     # Alter the scores only to be sorted by age. After we will resort it to be altered by a custom key for race We
     # had to remove the first row, due to Name Age etc messing up the sorting method, and i couldnt find a work
     # around to skip the first row.
-    sort_age = combined_scores.iloc[1:].copy()
+    sort_age = data
     # Create sort race for when we will use it in 5 minutes.( 1 millisecond when running)
     sort_race = sort_age.copy()
+    print(sort_race)
     # Sorts all the ages in the databse without having to transfer.
-    sort_age.sort_values(2, inplace=True)
+    sort_age.sort_values(by="Age", inplace=True)
+    print(sort_age)
     # This will find the amount of rows and subtract 1 so we keep the first row as (:-22) will remove 22 rows
     # starting from the bottom.
-    drop = len(combined_scores) - 1
-    top_of_file = combined_scores[:-drop]
     # Append it to sort_age
-    sort_age = top_of_file.append(sort_age)
     # Create a new Intake For age Sort
-    sort_age.to_excel(newPath + "Full Intakes-Scores-AgeSort.xlsx", header=False, index=False, na_rep='NA')
-    resize_columns(newPath + "Full Intakes-Scores-AgeSort.xlsx")
+    sort_age.to_excel(newPath + "Full Quizzes-Scores-AgeSort.xlsx", header=True, index=False, na_rep='NA')
+    resize_columns(newPath + "Full Quizzes-Scores-AgeSort.xlsx")
 
-    sort_race[4] = pd.Categorical(sort_race[4], ["W", "B", "L", "A", "NA", "O", "N/A"])
+    sort_race[4] = pd.Categorical(sort_race["Race"], ["W", "B", "L", "A", "NA", "O", "N/A"])
     sort_race.sort_values(4, inplace=True)
-    sort_race = top_of_file.append(sort_race)
 
-    sort_race.to_excel(newPath + "Full Intakes-Scores-RaceSort.xlsx", header=False, index=False, na_rep='NA')
-    resize_columns(newPath + "Full Intakes-Scores-RaceSort.xlsx")
+    sort_race.to_excel(newPath + "Full Quizzes-Scores-RaceSort.xlsx", header=True, index=False, na_rep='NA')
+    resize_columns(newPath + "Full Quizzes-Scores-RaceSort.xlsx")
+    print()
 
     # combined_scores.to_excel(directory + "/" + "Full Intakes-Scores.xlsx", header=False, index=False, na_rep='NA')
     # resize_columns(directory + "/" + "Full Intakes-Scores-AgeSort.xlsx")
-    demographics(newPath + "Full Intakes-Scores-RaceSort.xlsx","Race",)
-    demographics_number_groups(newPath + "Full Intakes-Scores-AgeSort.xlsx","Age",[29,40,55])
+    demographics(newPath + "Full Quizzes-Scores-RaceSort.xlsx", "Race", )
+    demographics_number_groups(newPath + "Full Quizzes-Scores-AgeSort.xlsx", "Age", [29, 40, 55])
 
     sys.exit(0)
